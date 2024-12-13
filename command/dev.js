261b95bd62cc67f486611affc84f336eee77d8bb
BlackMD445 <botabdo732@gmail.com>
2024-12-13 18:17:12 +0100

    Update and rename my-package.js to dev.js

---
+++ command/dev.js
@@ -0,0 +1,11 @@
+
+const config = require('../config');
+
+module.exports = {
+    name: "devinfo",
+    description: "عرض معلومات المطور",
+    async execute(client, message) {
+        message.reply(`رقم المطور: ${config.developer}`);
+    },
+};
+￼Enter
